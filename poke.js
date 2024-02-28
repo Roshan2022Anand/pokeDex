e505c76a01a71000fa693e10f976bec5a538c064
unknown <roshananand2005@gmail.com>
2024-02-28 22:16:55 +0530

    fixed some bugs and added new features like clickfeatures

--- poke.js
+++ poke.js
@@ -3,7 +3,7 @@ const search = document.getElementById("search-value");
 const srchBtn = document.getElementById("search-btn");
 const main = document.getElementById("main");
 const box = document.getElementById("box");
-const pokeImg = document.getElementById("poke-img");
+const pokeImgOutput = document.getElementById("poke-img-output");
 const pokeName = document.getElementById("poke-name");
 const backBtn = document.getElementById("back-btn");
 const pokeType = document.getElementById("poke-type");
@@ -14,15 +14,14 @@ const pokeSpAttack = document.getElementById("poke-sp-attack");
 const pokeSpDefense = document.getElementById("poke-sp-defense");
 const pokeSpeed = document.getElementById("poke-speed");
 //function to search pokemon from the search bar value
-srchBtn.addEventListener("click", searchPokemon = async () => {
-    let name = search.value;
-    if(name == "")
+const searchPokemon = async (myName) => {
+    if (myName == "")
         alert("are you mad ?");
-    else{
+    else {
         try {
-            const pokeFetch = await fetch(`https://pokeapi-proxy.freecodecamp.rocks/api/pokemon/${name}`);
+            const pokeFetch = await fetch(`https://pokeapi-proxy.freecodecamp.rocks/api/pokemon/${myName}`);
             const data = await pokeFetch.json();
-            pokeImg.setAttribute("src", data.sprites.front_default);
+            pokeImgOutput.setAttribute("src", data.sprites.front_default);
             pokeName.innerText = data.name;
             pokeType.innerText = data.types[0].type.name;
             pokeHp.innerText = data.stats[0].base_stat;
@@ -32,31 +31,37 @@ srchBtn.addEventListener("click", searchPokemon = async () => {
             pokeSpDefense.innerText = data.stats[4].base_stat;
             pokeSpeed.innerText = data.stats[5].base_stat;
             await box.showModal();
-            // console.log(data.stats[0].base_stat, data.stats[1], data.stats[2], data.stats[3], data.stats[4], data.stats[5]);
+            console.log(data);
         } catch (err) {
-            alert("please enter a valid name");
+            console.log(err)
+            // alert("please enter a valid name");
         }
     }
-})
+}
+srchBtn.addEventListener("click", () => { searchPokemon(search.value) });
 //function for back button 
-backBtn.addEventListener("click",()=>{
+backBtn.addEventListener("click", () => {
     box.close();
 })
+// const clickMe=(name)=>{
+//     searchPokemon(name);
+// }
 //function to display few pokemon
 const displayPokemon = (data) => {
     // console.log(data);
+    let newName = data.name;
     main.innerHTML += `
-    <div id="my-card">
-            <img src="${data.sprites.front_default}" id="poke-img" alt="...">
-            <div>
-                <h5>${data.types[0].type.name}</h5>
-                <p>${data.name}</p>
-            </div>
+    <div class="my-card"  onclick="searchPokemon('${newName}')">
+    <img src="${data.sprites.front_default}" id="poke-img" alt="...">
+     <div>
+      <h5>${data.types[0].type.name}</h5>
+      <p>${data.name}</p>
+     </div>
     </div>`
 }
-for (let i = 1; i < 100; i++) {
+for (let i = 1; i < 1000; i++) {
     // let randomPokemon = Math.ceil(Math.random() * 300);
     fetch(`https://pokeapi-proxy.freecodecamp.rocks/api/pokemon/${i}`)
-    .then((res) => res.json())
-    .then((data) => displayPokemon(data))
+        .then((res) => res.json())
+        .then((data) => displayPokemon(data))
 }
